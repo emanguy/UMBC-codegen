ab21198ce5f012408e525e06bef8d0eb7ba7f481
Evan Rittenhouse <emanguy1@gmail.com>
2015-04-11 20:31:01 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Adds the "cppbuild" command to the user's shell environment.
+# Part of the UMBC coding standards autogenerator by Evan Rittenhouse
+
+# This file assumes that the user cloned the git repository to their home directory.
+
+# Add command on run
+alias cppbuild="~/UMBC-codegen/MakeCppProject.sh"
+# Add command to shell startup, assuming normal users are using the TCSH shell
+echo 'alias cppbuild="~/UMBC-codegen/MakeCppProject.sh"' >> ~/.cshrc
